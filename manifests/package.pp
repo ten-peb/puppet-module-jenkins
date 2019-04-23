b66e53a544bab71c17f5aa0ac9efc7b63b37b2be
Peter L. Berghold <pberghold@tenna.com>
2019-04-23 17:42:06 +0000

    needed to import jenkins key

--- manifests/package.pp
+++ manifests/package.pp
@@ -5,12 +5,21 @@
 # @example
 #   include jenkins::package
 class jenkins::package {
-    file{'/etc/apt/sources.list.d/jenkins.list':
+
+  $key_wget='wget -q -O - https://pkg.jenkins.io/debian/jenkins-ci.org.key'
+  $key_import="${key_wget} | apt-key add -"
+  
+  exec {'import jenkins key':
+    command => $key_import,
+    path => '/bin:/usr/bin:/sbin:/usr/sbin:/usr/local/bin'
+  }
+  file{'/etc/apt/sources.list.d/jenkins.list':
     ensure  => present,
     content => template('profile/jenkins/jenkins.list.erb'),
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
+    require => Exec['import jenkins key'],
     notify  => Exec['refresh aptcache for jenkins']
   }
   exec{'refresh aptcache for jenkins':
